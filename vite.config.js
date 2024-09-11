c892d2154bb284e90ebab741b00c22cd424ddd22
Saihaj Mann <saihajmann07@gmail.com>
2024-09-11 11:20:49 -0600

    deployment correction

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from "vite";
 
+//change base fix vercel deployment?
+// /solar-system-threejs
 export default defineConfig({
-  base: "/solar-system-threejs",
+  base: "",
 });
